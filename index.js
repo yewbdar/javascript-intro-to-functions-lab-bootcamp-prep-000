813697efb1d32655aadbee39bcba4fff3e9b0db0
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-07-02 19:49:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(str){
     return "I can't hear you!"
   }else if(str === str.toUpperCase()){
     return "YES INDEED!"
-  }else if(str === "I love you, Grandma  "){
+  }else if(str === "I love you, Grandma."){
     return "I love you,too"
   }
 }
