d4948a756ab446137de085e9e3af7fb8caad1ea9
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-07-02 19:46:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function logShout(str){
 function logWhisper(str){
   console.log(str.toLowerCase())
 }
+function sayHiToGrandma(str){
+  if(str === str.toLowerCase()){
+    return "I can't hear you!"
+  }else if(str === str.toUpperCase()){
+    return "YES INDEED!"
+  }
+}
